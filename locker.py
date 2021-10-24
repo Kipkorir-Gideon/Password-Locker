563a814f594344b8c189792ddc3cae037bec40e8
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-24 12:17:07 +0300

    Add methods to create save and display credentials.

--- locker.py
+++ locker.py
@@ -21,3 +21,30 @@ def checks_user(user_name, password):
     '''
     checking_user = Credentials.check_user(user_name, password)
     return checking_user
+
+def generate_password():
+    '''
+    A method to generate password
+    '''
+    generate_pass = Credentials.generate_password()
+    return generate_pass
+
+def create_credentials(user_name, account_name, password):
+    '''
+    A method to create user credentials
+    '''
+    new_credential = Credentials(user_name, account_name, password)
+    return new_credential
+
+def save_credentials(credentials):
+    '''
+    A method to save new credentials
+    '''
+    Credentials.save_credential(credentials)
+
+def display_credentials(user_name):
+    '''
+    A method to display user's credential
+    '''
+    return Credentials.display_credentials(user_name)
+    
